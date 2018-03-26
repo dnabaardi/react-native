ce336668e83828ef16454db669ae5bf31a151718
dnabaardi <36947481+dnabaardi@users.noreply.github.com>
2018-03-26 22:14:26 +0800

    Update App.js

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import {
 } from 'react-native';
 import Layout from './src/Latihan/Layout';
 import VolumeBalok from './src/Latihan/VolumeBalok';
+import Cuaca from './src/Cuaca/Cuaca';
 
 
 export default class App extends Component {
